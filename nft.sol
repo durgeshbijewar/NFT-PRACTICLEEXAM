e7417ac884f337bf7bcd1e8f0cf1d364e31fcce2
durgeshbijewar <137867028+durgeshbijewar@users.noreply.github.com>
2024-01-12 04:49:52 -0500

    Add files via upload

---
+++ nft.sol
@@ -0,0 +1,20 @@
+// Write a Solidity Smart Contract for a Complete ERC721
+//Smart Contract where you can perform Minting the NFT
+//by paying in Ethers, Limiting the supply of NFT’s,
+//Allowing list functionality, Modifying the Minting
+//functionality, Withdrawing functionality, and, clean up
+//code by using internalMint() function.
+
+//ans=create a smart contract create a file nftproject
+//implement the code write smart contract
+//install @openzepline
+//install  ERC721
+//buying and selling the digital asset converting into blockchain based
+//buyin and selling is called as NFT mining
+//npx hardhat compile
+//npx hardhat run/script/deply.js
+//npm install @ache/alchemy-web3
+//create new file mint.js
+//create new file nftmetadata.js
+//comple the smart contract
+//nodescript/mint-nft.js
